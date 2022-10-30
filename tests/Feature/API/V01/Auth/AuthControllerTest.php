24b4d3edb7fd28f177c846f24e90eab39a609097
hadi-pd <h7ad3i@gmail.com>
2022-10-30 17:42:41 +0330

    Add login Test

--- tests/Feature/API/V01/Auth/AuthControllerTest.php
+++ tests/Feature/API/V01/Auth/AuthControllerTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature\API\V01\Auth;
 
+use App\Models\User;
+use Database\Factories\UserFactory;
+use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Nette\Utils\Random;
@@ -9,20 +12,45 @@ use Tests\TestCase;
 
 class AuthControllerTest extends TestCase
 {
-    use RefreshDatabase;
+    //  use RefreshDatabase;
+    
+    
+    /**
+     * Test Register
+     */
     public function test_register_should_be_validate()
     {
-        $response = $this->post('api/v1/auth/register');
-        $response->assertStatus(302);
+        $response = $this->postJson(route('auth.register'));
+        $response->assertStatus(422);
     }
     public function test_new_user_can_register()
     {
-            $test_code = random_int(10, 99).time();
-            $response = $this->post('api/v1/auth/register', [
+            // $test_code = random_int(10, 99).time();
+            $test_code = 22;
+            $response = $this->postJson(route('auth.register'), [
             'name' => "Hadi Poushedar".$test_code,
             'email' => $test_code."h7ad3i@gmail.com",
             'password'=> "12345678"
         ]);
         $response->assertStatus(201);
+    }
+    /**
+     * Test Login
+     */
+    public function test_login_should_be_validate()
+    {
+        $response = $this->postJson(route('auth.login'));
+        $response->assertStatus(422); 
+    }
+
+    public function test_user_can_login_with_true_credentials()
+    {
+        $test_code = 22;
+        // $user = factory(User::class)->create();
+        $response = $this->postJson(route('auth.login'),[
+            'email' => $test_code."h7ad3i@gmail.com",
+            'password'=> "12345678"
+        ]);
+        $response->assertStatus(200);
     }
 }
