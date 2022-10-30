a8c2e21fea15ae6ff1f6ce0efa9a6ebfe4d6eea0
hadi-pd <h7ad3i@gmail.com>
2022-10-30 17:42:25 +0330

    add login api route

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/auth')->group(function(){
     // Route::post('/register', 'API\AuthController@register');
-    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 });
